0624fd2e6d8438def9886da353aa74445506fb1f
Dang Khoa Huynh <122362962+MichalKhoa@users.noreply.github.com>
2023-01-26 17:06:42 +0100

    Rewritten using functions

--- main.py
+++ main.py
@@ -8,48 +8,78 @@ To compare runtime of the code
 '''
 
 file = open("sample-data.json", "r")
+loaded_data = json.load(file)
 
 MongoDBlink = input('Paste a link to the MongoDB db:')
 cluster = pymongo.MongoClient(MongoDBlink)
 db = cluster['test_database']
 col = db["test_output"]
 
-data = json.load(file)
 
-for i in range(len(data)):
-    post = dict()
-    post['name'] = data[i].get("name")
+def get_name(data):
+    return data.get("name")
+
+
+def get_cpu_usage(data):
     try:
-        post['cpu usage'] = data[i]["state"]["cpu"].get("usage")
+        return data["state"]["cpu"].get("usage")
     except TypeError:
-        post['cpu usage'] = 0
-
+        return 0
+    
+    
+def get_memory_usage(data):
     try:
-        post['memory usage'] = data[i]["state"]["memory"].get("usage")
+        return data["state"]["memory"].get("usage")
     except TypeError:
-        post['memory usage'] = 0
+        return 0
+    
 
-    post['status'] = data[i].get("status")
+def get_status(data):
+    return data.get("status")
 
-    post['created_at'] = datetime.timestamp(datetime.strptime(data[i].get("created_at"), "%Y-%m-%dT%H:%M:%S%z"))
 
+def get_created_at(data):
+    return datetime.timestamp(datetime.strptime(data.get("created_at"), "%Y-%m-%dT%H:%M:%S%z"))
+
+
+def get_ip_addresses(data):
     try:
-        list_of_keys = list(data[i]["state"]["network"].keys())
+        list_of_keys = list(data["state"]["network"].keys())
         ip_addresses = []
         for j in range(len(list_of_keys)):
-            for k in range(len(data[i]["state"]["network"][list_of_keys[j]]["addresses"])):
+            for k in range(len(data["state"]["network"][list_of_keys[j]]["addresses"])):
                 try:
-                    ip_addresses.append(data[i]["state"]["network"][list_of_keys[j]]["addresses"][k]["address"])
+                    ip_addresses.append(data["state"]["network"][list_of_keys[j]]["addresses"][k]["address"])
                 except TypeError:
                     continue
                 except IndexError:
-                    ip_addresses.append(data[i]["state"]["network"][list_of_keys[j]]["addresses"]["address"])
+                    ip_addresses.append(data["state"]["network"][list_of_keys[j]]["addresses"]["address"])
                     break
-        post['assigned IP addressses'] = ip_addresses
+        return ip_addresses
     except TypeError:
-        post['assigned IP addressses'] = "No assigned IP addresses"
+        return "No assigned IP addresses"
 
-    col.insert_one(post)
+
+def parse_and_extract(data):
+    for i in range(len(data)):
+        post = dict()
+
+        post['name'] = get_name(data[i])
+        post['cpu usage'] = get_cpu_usage(data[i])
+        post['memory usage'] = get_memory_usage(data[i])
+        post['status'] = get_status(data[i])
+        post['created_at'] = get_created_at(data[i])
+        post['assigned IP addresses'] = get_ip_addresses(data[i])
+
+        col.insert_one(post)
+
+
+def main():
+    parse_and_extract(loaded_data)
+
+
+if __name__ == '__main__':
+    main()
 
 end_time = datetime.now()
 print('\nDuration: {}'.format(end_time - start_time))
